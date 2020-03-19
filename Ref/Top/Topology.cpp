f2ab667fbbac8b339a65af461a307384f3fffdfe
mdj0025 <mdj0025@tigermail.auburn.edu>
2020-03-19 14:31:03 -0500

    added MathComponent tutorial to Ref dir

--- Ref/Top/Topology.cpp
+++ Ref/Top/Topology.cpp
@@ -171,6 +171,18 @@ Ref::SignalGen SG5
 #endif
 ;
 
+Ref::MathSenderComponentImpl mathSender
+#if FW_OBJECT_NAMES == 1
+    ("mathSender")
+#endif
+;
+
+Ref::MathReceiverComponentImpl mathReceiver
+#if FW_OBJECT_NAMES == 1
+    ("mathReceiver")
+#endif
+;
+
 Svc::AssertFatalAdapterComponentImpl fatalAdapter
 #if FW_OBJECT_NAMES == 1
 ("fatalAdapter")
@@ -253,6 +265,9 @@ void constructApp(int port_number, char* hostname) {
 	fatalHandler.init(0);
 	health.init(25,0);
 	pingRcvr.init(10);
+	
+	mathSender.init(10,0);
+    mathReceiver.init(10,0);
     // Connect rate groups to rate group driver
     constructRefArchitecture();
 
@@ -272,10 +287,15 @@ void constructApp(int port_number, char* hostname) {
 	health.regCommands();
 	pingRcvr.regCommands();
 
+    mathSender.regCommands();
+    mathReceiver.regCommands();
+
     // read parameters
     prmDb.readParamFile();
     recvBuffComp.loadParameters();
     sendBuffComp.loadParameters();
+
+    mathReceiver.loadParameters();
 
     // set health ping entries
 
@@ -316,6 +336,8 @@ void constructApp(int port_number, char* hostname) {
     fileUplink.start(0, 100, 10*1024);
 
     pingRcvr.start(0, 100, 10*1024);
+
+    mathSender.start(0, 100, 10*1024);
 
     // Initialize socket server
     sockGndIf.startSocketTask(100, 10*1024, port_number, hostname, Svc::SocketGndIfImpl::SEND_UDP);
@@ -367,6 +389,8 @@ void exitTasks(void) {
     fileUplink.exit();
     fileDownlink.exit();
     cmdSeq.exit();
+
+    mathSender.exit();
 }
 
 void print_usage() {
--- mk/configs/modules/modules.mk
+++ mk/configs/modules/modules.mk
@@ -92,7 +92,11 @@ REF_MODULES := \
 	Ref/RecvBuffApp \
 	Ref/SendBuffApp \
 	Ref/SignalGen \
-	Ref/PingReceiver
+	Ref/PingReceiver \
+	Ref/MathPorts \
+	Ref/MathTypes \
+	Ref/MathSender \
+	Ref/MathReceiver
 	
 Ref_MODULES := \
 	\
